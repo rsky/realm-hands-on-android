8534924c3b35e0059628b15434ff3ff05da08da0
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2016-04-09 16:16:10 +0900

    migration

--- app/src/main/java/io/realm/handson2/twitter/MyApplication.java
+++ app/src/main/java/io/realm/handson2/twitter/MyApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
+import io.realm.RealmObjectSchema;
 import twitter4j.TwitterFactory;
 
 public class MyApplication extends Application {
@@ -18,8 +19,21 @@ public class MyApplication extends Application {
                 "<consumerKey>",
                 "<consumerSecret>");
 
-        Realm.setDefaultConfiguration(new RealmConfiguration.Builder(this)
-                //.deleteRealmIfMigrationNeeded()
-                .build());
+        Realm.setDefaultConfiguration(buildMigration());
+    }
+
+    private RealmConfiguration buildMigration() {
+        return new RealmConfiguration.Builder(this)
+                .schemaVersion(1L)
+                .migration((realm, oldVersion, newVersion) -> {
+                    if (oldVersion == 0L) {
+                        final RealmObjectSchema tweetSchema = realm.getSchema().get("Tweet");
+                        tweetSchema.addField("favorited", boolean.class);
+
+                        // noinspection UnusedAssignment
+                        oldVersion++;
+                    }
+                })
+                .build();
     }
 }
