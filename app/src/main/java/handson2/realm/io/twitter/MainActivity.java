19a4fcdafe84442188a9be108d0bad5991851a75
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2016-04-09 13:23:14 +0900

    initial commmit

---
+++ app/src/main/java/handson2/realm/io/twitter/MainActivity.java
@@ -0,0 +1,13 @@
+package handson2.realm.io.twitter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
