b45490e2b41962244339b079864ee959ac9e29ce
Ryusuke SEKIYAMA <rsky0711@gmail.com>
2016-04-09 17:16:52 +0900

    implement logout

--- app/src/main/java/io/realm/handson2/twitter/MainActivity.java
+++ app/src/main/java/io/realm/handson2/twitter/MainActivity.java
@@ -1,6 +1,7 @@
 package io.realm.handson2.twitter;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -9,6 +10,11 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.CookieManager;
+import android.webkit.CookieSyncManager;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,9 +47,31 @@ public class MainActivity extends AppCompatActivity {
             case R.id.refresh:
                 startService(new Intent(this, UpdateService.class));
                 return true;
+            case R.id.logout:
+                logout();
+                startActivity(new Intent(this, LoginActivity.class));
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void logout() {
+        TwitterAuthUtil.clearTokens();
+
+        CookieManager cookieManager = CookieManager.getInstance();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            cookieManager.removeSessionCookies(null);
+            cookieManager.flush();
+        } else {
+            // noinspection deprecation
+            CookieSyncManager.createInstance(this);
+            // noinspection deprecation
+            cookieManager.removeSessionCookie();
+        }
+
+        final Twitter twitter = TwitterFactory.getSingleton();
+        twitter.setOAuthAccessToken(null);
     }
 
     private static final class MainPagerAdapter extends FragmentStatePagerAdapter {
